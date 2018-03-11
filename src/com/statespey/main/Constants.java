f1fbb7c2b51a33a59e988f1dabb5e1434ed02839
Family <Family@DESKTOP-K783EQS.fios-router.home>
2018-03-11 19:36:47 -0400

    Added Constants.java

---
+++ src/com/statespey/main/Constants.java
@@ -0,0 +1,9 @@
+package com.statespey.main;
+
+public final class Constants {
+
+	public static final String CLIENT_ID = "IG_CLIENT_ID";
+	public static final String CLIENT_SECRET = "IG_CLIENT_SECRET";
+	public static final String REDIRECT_URI = "IG_REDIRECT_URL";
+
+}
