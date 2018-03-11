d9c2ca17c3b007e0a017ec9a97d4ed3e97ff3bc4
Family <Family@DESKTOP-K783EQS.fios-router.home>
2018-03-11 19:12:18 -0400

    Fixed up some (hopefully) useless packages and made it more neat.

---
+++ src/com/statespey/main/Window.java
@@ -0,0 +1,21 @@
+package com.statespey.main;
+
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class Window {
+
+	JFrame frame = new JFrame();
+
+	public static void main(String[] args) {
+		Window window = new Window();
+		window.frame.setIconImage(Toolkit.getDefaultToolkit().getImage("resources\\icon.png"));
+		window.frame.setVisible(true);
+		window.frame.setResizable(false);
+		window.frame.setTitle("StatEspey - Viral Video Tracker for Instagram");
+		window.frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		window.frame.setAlwaysOnTop(true); 
+
+	}
+}
