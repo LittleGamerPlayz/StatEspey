de88f8ac1a081c8139b07d7ff27fd35d0745fcd4
Family <Family@DESKTOP-K783EQS.fios-router.home>
2018-03-11 17:28:21 -0400

    Added icon to JFrame window.

--- StatEspey/src/com/statespey/main/Window.java
+++ StatEspey/src/com/statespey/main/Window.java
@@ -2,19 +2,25 @@ package com.statespey.main;
 
 import java.awt.Toolkit;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Window {
 
 	JFrame frame = new JFrame();
 
-	public static void main(String args) {
+	public Window() {
+
+	}
+
+	public static void main(String[] args) {
 		Window window = new Window();
+		window.frame.setIconImage(Toolkit.getDefaultToolkit().getImage("res\\icon.png"));
 		window.frame.setVisible(true);
 		window.frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
 		window.frame.setResizable(false);
 		window.frame.setTitle("StatEspey - Viral Video Tracker for Instagram");
+		window.frame.isUndecorated();
 
 	}
-
 }
